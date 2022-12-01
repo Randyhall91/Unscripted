4d72e68a97c6b17ff6a984546a14689aa149329c
Thesommoner <randyrocks11@gmail.com>
2022-12-01 14:29:53 -0700

    test gitignore on axios file

--- kieras_photography.client/src/models/Post.js
+++ kieras_photography.client/src/models/Post.js
@@ -3,10 +3,10 @@
 
 export class Post{
   constructor(data){
-    this.id = data.attributes.Post[0].id
-    this.title = data.attributes.Post[0].Title
-    this.content = data.attributes.Post[0].Content
-    this.headerImg = data.attributes.Post[0].Header.data.attributes.formats.medium.url
+    this.id = data.attributes.Post.id
+    this.title = data.attributes.Post.Title
+    this.content = data.attributes.Post.Content
+    this.headerImg = data.attributes.Post.Header.data.attributes.formats.medium.url
     // this.additionalImgs = data[0].attributes.Post[0].AdditionalImgs.data.attributes.formats.medium
     
   }
