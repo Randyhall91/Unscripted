493d9b4bb5db14e788cf8892ace6f9212e06ae80
Thesommoner <randyrocks11@gmail.com>
2022-12-01 14:33:26 -0700

    got bearer token in .env

--- kieras_photography.client/src/services/AxiosService.js
+++ kieras_photography.client/src/services/AxiosService.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
-import { baseURL } from '../env'
+import { baseURL, bearer } from '../env'
 export const api = Axios.create({
   baseURL,
   timeout: 8000,
-  headers: {Authorization: `Bearer d2fb1bbe19fe6c2a047d729764e6baa584207b790ea5212e272c2ffbf19797cccc7bc1eb9c769548cc48f485dbabbc8bc8e39362b417e24f1e7d2b4dc625fa1c5a598ea3931b3ae397be020ba8c06ad26cbd380906bea9882793837ab9ebc66aea1cc14fbf71dd13bfc55874b9a719e8d87f3d1c974ef0aa5e76f3d40d652507`}
+  headers: {Authorization: `Bearer ${bearer}`}
 })
 
