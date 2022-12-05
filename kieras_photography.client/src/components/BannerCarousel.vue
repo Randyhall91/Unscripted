c0047acc23b89253e20d4778c3040b99f6a690f1
Randyhall91 <randyrocks11@gmail.com>
2022-12-05 12:48:12 -0700

    banner adding index to carousel

--- kieras_photography.client/src/components/BannerCarousel.vue
+++ kieras_photography.client/src/components/BannerCarousel.vue
@@ -2,14 +2,15 @@
   <div id="bannerCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
 
     <div class="carousel-inner carouselSize py-3">
-      <div v-for="img in home.firstCarouselImg" data-bs-interval="1000" class="carousel-item active">
+      <div v-for="(img, index) in home.firstCarouselImg" :data-id="index" data-bs-interval="1000"
+        class="carousel-item active">
         <img :src="(baseURL + img.medium.url)" class="carousel-img img-fit d-block" :alt="img.medium.name">
         <div class="carousel-text">
           <h1>Treasure Valley</h1>
           <h1>LifeStyle Family Photography</h1>
         </div>
       </div>
-      <div v-for="img in home.carouselImgs" data-bs-interval="1000" class="carousel-item">
+      <div v-for="(img, index) in home.carouselImgs" :data-id="index" data-bs-interval="1000" class="carousel-item">
         <img :src="(baseURL + img.medium.url)" class="carousel-img img-fit d-block" :alt="img.medium.name">
         <div class="carousel-text">
           <h1>Treasure Valley</h1>
